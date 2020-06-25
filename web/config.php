c26853b6a579f08aa15531d654f406d17628b5c7
lux-lu <67232749+lux-lu@users.noreply.github.com>
2020-06-25 14:18:10 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,9 +1,9 @@
 <?php
 define('ROOT_PART', Root_part());
 define('APIKEY', 'AIzaSyC4a_vjepaxFUxaWpa8kTyeBixK2BHcLyM');
-define('GJ_CODE', getenv("jp"));
-define('SITE_NAME',getenv("youtube"));
-define('TITLENAME', getenv("YOUTUBE"));
+define('GJ_CODE', 'jp');
+define('SITE_NAME','y2');
+define('TITLENAME', 'Y2');
 define('EN2DEKEY',getenv("hello123456789"));
 define('EMAIL', getenv("ch1366231@gmail.com"));
 ?>
